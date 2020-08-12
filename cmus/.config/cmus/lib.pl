922ab809d587bae24eccf894b2a05f2b5f70a935
Yassine Bridi <ybridi@gmail.com>
2020-08-12 01:37:11 +0100

    Add a music server and player

---
+++ cmus/.config/cmus/lib.pl
@@ -0,0 +1,133 @@
+/home/yaslix/mounting/YassineHD/Music/Adele- Live at Royal Albert Hall - [HD FULL CONCERT].mp4
+/home/yaslix/mounting/YassineHD/Music/pretty things - thousand ages from the sun.wmv.mp3
+/home/yaslix/mounting/YassineHD/Music/Strawberry Tapes - Brand new cut.mp3
+/home/yaslix/mounting/YassineHD/Music/Strawberry Tapes - Too Woke To Fly.mp3
+/home/yaslix/mounting/YassineHD/Music/Stromae - Racine Carrée (Live Full Concert).mp4
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/01. Sunrise.mp3
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/02. Church.mp3
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/03. Trouble In Town.mp3
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/04. BrokEn.mp3
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/05. Daddy.mp3
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/06. WOTWPOTP.mp3
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/07. Arabesque.mp3
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/08. When I Need A Friend.mp3
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/09. SunriseSunset Interlude 1.mp3
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/10. SunriseSunset Interlude 2.mp3
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/11. SunriseSunset Interlude 3.mp3
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/12. SunriseSunset Interlude 4.mp3
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/13. SunriseSunset Interlude 5.mp3
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/14. SunriseSunset Interlude 6.mp3
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/15. Guns.mp3
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/16. Orphans.mp3
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/17. Eko.mp3
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/18. Cry Cry Cry.mp3
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/19. Old Friends.mp3
+/home/yaslix/mounting/YassineHD/Music/Coldplay - Everyday Life/20. Bani Adam.mp3
+/home/yaslix/mounting/YassineHD/Music/RAMMSTEIN (2019)/01 DEUTSCHLAND.mp3
+/home/yaslix/mounting/YassineHD/Music/RAMMSTEIN (2019)/02 RADIO.mp3
+/home/yaslix/mounting/YassineHD/Music/RAMMSTEIN (2019)/03 ZEIG DICH.mp3
+/home/yaslix/mounting/YassineHD/Music/RAMMSTEIN (2019)/04 AUSLÄNDER.mp3
+/home/yaslix/mounting/YassineHD/Music/RAMMSTEIN (2019)/05 SEX.mp3
+/home/yaslix/mounting/YassineHD/Music/RAMMSTEIN (2019)/06 PUPPE.mp3
+/home/yaslix/mounting/YassineHD/Music/RAMMSTEIN (2019)/07 WAS ICH LIEBE.mp3
+/home/yaslix/mounting/YassineHD/Music/RAMMSTEIN (2019)/08 DIAMANT.mp3
+/home/yaslix/mounting/YassineHD/Music/RAMMSTEIN (2019)/09 WEIT WEG.mp3
+/home/yaslix/mounting/YassineHD/Music/RAMMSTEIN (2019)/10 TATTOO.mp3
+/home/yaslix/mounting/YassineHD/Music/RAMMSTEIN (2019)/11 HALLOMANN.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2008 - Tame Impala (EP)/[01] Desire Be, Desire Go.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2008 - Tame Impala (EP)/[02] Skeleton Tiger.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2008 - Tame Impala (EP)/[03] Half Full Glass Of Wine.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2008 - Tame Impala (EP)/[04] Forty One Mosquitoes Flying In Formation.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2008 - Tame Impala (EP)/[05] Slide Through My Fingers.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2008 - Tame Impala (EP)/[06] Wander.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2008 - Tame Impala H.I.T.S. (EP)/01. H.F.G.W (Canyons Drunken Rage).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2008 - Tame Impala H.I.T.S. (EP)/02. Skeleton Tiger (Retamed By Canyons).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2008 - Tame Impala H.I.T.S. (EP)/03. The Sun (Fred Cherry Eclipse).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2009 - Sundown Syndrome - Remember Me (EP)/[03] Sundown Syndrome (Canyons Remix).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2009 - Sundown Syndrome - Remember Me (EP)/[01] Sundown Syndrome.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2009 - Sundown Syndrome - Remember Me (EP)/[02] Remember Me.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2010 - Live At The Corner/[01] Sundown Syndrome (Live At The Corner).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2010 - Live At The Corner/[02] Skeleton Tiger (Live At The Corner).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2010 - Live At The Corner/[03] The Bold Arrow Of Time (Live At The Corner).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2010 - Live At The Corner/[04] Half Full Glass Of Wine (Live At The Corner).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/ExtraSpeaker/[01] Sundown Syndrome.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/ExtraSpeaker/[02] Remember Me.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/ExtraSpeaker/[03] Half Full Glass of Wine.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/ExtraSpeaker/[04] Wander.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/ExtraSpeaker/[05] Why Wont You Make Up Your Mind (Erol Alkan Rework).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/ExtraSpeaker/[06] Lucidity (Pilooski Remix).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/ExtraSpeaker/[07] Solitude Is Bliss (Mickey Moonlight T.A.M Remix).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/ExtraSpeaker/[08] 41 Mojitos (Canyons Poolside Dub).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/ExtraSpeaker/[09] Canyons Sunrise Reprise.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/[01] It Is Not Meant To Be.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/[02] Desire Be Desire Go.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/[03] Alter Ego.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/[04] Lucidity.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/[05] Why Wont You Make Up Your Mind.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/[06] Solitude Is Bliss.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/[07] Jeremys Storm.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/[08] Expectation.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/[09] The Bold Arrow Of Time.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/[10] Runway, Houses, City, Clouds.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/[11] I Dont Really Mind.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - InnerSpeaker (Deluxe Limited Edition)/[12] 30 Mins With Mathew Saville (Secret Bonus Track).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - Solitude Is Bliss (EP)/[01] Solitude Is Bliss (Original).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - Solitude Is Bliss (EP)/[02] Solitude Is Bliss (Time  Space Machine Remix).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - Solitude Is Bliss (EP)/[03] Solitude Is Bliss (Midnight Juggernauts Remix).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - Solitude Is Bliss (EP)/[04] Solitude Is Bliss (Mickey Moonlight T.A.M. Remix).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - Solitude Is Bliss (EP)/[05] Solitude Is Bliss (Mickey Moonlight Remix).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - Solitude Is Bliss (EP)/[06] Solitude Is Bliss (Canyons Dow Jones Mix).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - Why Wont You Make Up Your Mind (EP)/[01] Why Wont You Make Up Your Mind (Erol Alkan Rework).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - Why Wont You Make Up Your Mind (EP)/[02] Why Wont You Make Up Your Mind (Erol Alkan Instrumental).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2011 - Why Wont You Make Up Your Mind (EP)/[03] Lucidity (Pilooski Remix).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Elephant (EP)/[01] Elephant.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Elephant (EP)/[02] Elephant (Todd Rundgren Remix).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Elephant (EP)/[03] Elephant (Canyons Wooly Mammoth Extension).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Lonerism (Deluxe Limited Edition)/[01] Be Above It.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Lonerism (Deluxe Limited Edition)/[02] Endors Toi.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Lonerism (Deluxe Limited Edition)/[03] Apocalypse Dreams.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Lonerism (Deluxe Limited Edition)/[04] Mind Mischief.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Lonerism (Deluxe Limited Edition)/[05] Music To Walk Home By.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Lonerism (Deluxe Limited Edition)/[06] Why Wont They Talk To Me-.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Lonerism (Deluxe Limited Edition)/[07] Feels Like We Only Go Backwards.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Lonerism (Deluxe Limited Edition)/[08] Keep On Lying.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Lonerism (Deluxe Limited Edition)/[09] Elephant.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Lonerism (Deluxe Limited Edition)/[10] She Just Wont Believe Me.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Lonerism (Deluxe Limited Edition)/[11] Nothing That Has Happened So Far Has Been Anything We Could Control.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Lonerism (Deluxe Limited Edition)/[12] Suns Coming Up.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Lonerism (Deluxe Limited Edition)/Rough Trade Bonus Disc/[01] Apocalypse Dreams (Live).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Lonerism (Deluxe Limited Edition)/Rough Trade Bonus Disc/[02] Elephant (Live).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Lonerism (Deluxe Limited Edition)/Rough Trade Bonus Disc/[03] Feels Like We Only Go Backwards (Live).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Lonerism (Deluxe Limited Edition)/Rough Trade Bonus Disc/[04] Elephant (Todd Rundgren Remix).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Lonerism (Deluxe Limited Edition)/Rough Trade Bonus Disc/[05] Elephant (Canyons Wooly Mammoth Extension).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Lonerism (Deluxe Limited Edition)/Vinyl 7/[01] A - Led Zeppelin.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2012 - Lonerism (Deluxe Limited Edition)/Vinyl 7/[02] B - Beverly Laurel.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2013 - Mind Mischief (EP)/[01] Mind Mischief.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2013 - Mind Mischief (EP)/[02] Mind Mischief (Ducktails Remix).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2013 - Mind Mischief (EP)/[03] Mind Mischief (The Field Remix).mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2013 - Peace and Paranoia Tour 2013/[01] The Flaming Lips - Runway, Houses, City, Clouds.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2013 - Peace and Paranoia Tour 2013/[02] The Flaming Lips - Elephant.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2013 - Peace and Paranoia Tour 2013/[03] Tame Impala - Are You A Hypnotist.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2013 - Peace and Paranoia Tour 2013/[04] Tame Impala - Silver Trembling Hands.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2014 - Live Versions/[01] Endors Toi.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2014 - Live Versions/[02] Why Wont You Make Up Your Mind.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2014 - Live Versions/[03] Sestri Levante.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2014 - Live Versions/[04] Mind Mischief.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2014 - Live Versions/[05] Desire Be, Desire Go.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2014 - Live Versions/[06] Half Full Glass of Wine.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2014 - Live Versions/[07] Be Above It.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2014 - Live Versions/[08] Feels Like We Only Go Backwards.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2014 - Live Versions/[09] Apocalypse Dreams.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2015 - Currents/01. Let It Happen.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2015 - Currents/02. Nangs.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2015 - Currents/03. The Moment.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2015 - Currents/04. Yes Im Changing.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2015 - Currents/05. Eventually.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2015 - Currents/06. Gossip.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2015 - Currents/07. The Less I Know The Better.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2015 - Currents/08. Past Life.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2015 - Currents/09. Disciples.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2015 - Currents/10. Cause Im A Man.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2015 - Currents/11. Reality In Motion.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2015 - Currents/12. Love Paranoia.mp3
+/home/yaslix/mounting/YassineHD/Music/Tame Impala/2015 - Currents/13. New Person, Same Old Mistakes.mp3
