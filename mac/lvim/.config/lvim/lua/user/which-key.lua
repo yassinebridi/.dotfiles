74415e69cafe875d24b4cec2369a89997d13d372
Yassine Bridi <ybridi@gmail.com>
2021-09-12 16:32:18 +0100

    Lunvarvim: Better save and quite, Better find files and find text

--- mac/lvim/.config/lvim/lua/user/which-key.lua
+++ mac/lvim/.config/lvim/lua/user/which-key.lua
@@ -2,18 +2,14 @@
 -- Which Key --
 ---------------
 
--- lvim.builtin.which_key.setup.plugins.presets = {
---   g = false,
--- }
--- Use which-key to add extra bindings with the leader-key prefix
-lvim.builtin.which_key.mappings["f"] = { "<cmd>:lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git', '-g', '!node_modules', '-g', '!.next' }})<CR>", "Files" }
--- lvim.builtin.which_key.mappings["t"] = {
---   name = "+Trouble",
---   r = { "<cmd>Trouble lsp_references<cr>", "References" },
---   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnosticss" },
---   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
---   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnosticss" },
--- }
+-- Quiting
+lvim.builtin.which_key.mappings = {
+  ["q"] = {":q<CR>", "Quit"},
+  ["w"] = {":w<CR>", "Save"}
+}
+-- Better Searching
+lvim.builtin.which_key.mappings["f"] = { "<cmd>:lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git', '-g', '!node_modules', '-g', '!.next' }})<CR>", "Find Files" }
+
+-- Better Live Grep
+lvim.builtin.which_key.mappings["rg"] = { ":lua require'telescope.builtin'.live_grep({ vimgrep_arguments = { 'rg', '--hidden','--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case', '-u', '-g', '!yarn.lock', '-g', '!.git', '-g', '!node_modules', '-g', '!.next' } })<cr>", "Find Text" }
 
