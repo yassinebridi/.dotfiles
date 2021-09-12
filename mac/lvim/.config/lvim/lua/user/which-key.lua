5e541327fb1f9cf2729e0bcc11b959f5e239ff6f
Yassine Bridi <ybridi@gmail.com>
2021-09-12 16:34:32 +0100

    Lunarvim: quite buffer

--- mac/lvim/.config/lvim/lua/user/which-key.lua
+++ mac/lvim/.config/lvim/lua/user/which-key.lua
@@ -11,5 +11,8 @@ lvim.builtin.which_key.mappings = {
 lvim.builtin.which_key.mappings["f"] = { "<cmd>:lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git', '-g', '!node_modules', '-g', '!.next' }})<CR>", "Find Files" }
 
 -- Better Live Grep
-lvim.builtin.which_key.mappings["rg"] = { ":lua require'telescope.builtin'.live_grep({ vimgrep_arguments = { 'rg', '--hidden','--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case', '-u', '-g', '!yarn.lock', '-g', '!.git', '-g', '!node_modules', '-g', '!.next' } })<cr>", "Find Text" }
+lvim.builtin.which_key.mappings["rg"] = { "<cmd>:lua require'telescope.builtin'.live_grep({ vimgrep_arguments = { 'rg', '--hidden','--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case', '-u', '-g', '!yarn.lock', '-g', '!.git', '-g', '!node_modules', '-g', '!.next' } })<CR>", "Find Text" }
+
+-- Quite buffer
+lvim.builtin.which_key.mappings["c"] = { ":bd<CR>", "Quite buffer" }
 
