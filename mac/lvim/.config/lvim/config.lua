dfe524ffedb7fea8bbd421a8c4f4801d7e8e20c7
Yassine Bridi <ybridi@gmail.com>
2021-09-19 19:28:07 +0100

    Lunarvim: Remove Catpuuccino color scheme

--- mac/lvim/.config/lvim/config.lua
+++ mac/lvim/.config/lvim/config.lua
@@ -11,11 +11,10 @@ lvim.plugins = {
   {"jose-elias-alvarez/nvim-lsp-ts-utils", requires = { "jose-elias-alvarez/null-ls.nvim" }},
   {"JoosepAlviste/nvim-ts-context-commentstring"},
   {"windwp/nvim-ts-autotag"},
-  {"Pocco81/Catppuccino.nvim"},
   {"rmagatti/auto-session"},
   {"ellisonleao/gruvbox.nvim", requires = { "rktjmp/lush.nvim" }},
   {"terryma/vim-multiple-cursors"},
-  {"pantharshit00/vim-prisma"},
   {"norcalli/nvim-colorizer.lua"},
+  {"pantharshit00/vim-prisma"},
   {"jparise/vim-graphql"}
 }
