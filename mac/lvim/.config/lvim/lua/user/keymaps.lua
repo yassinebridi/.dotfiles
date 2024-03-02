ccff3140ae57e43c57df3510943ad3f62dc76f82
Yassine Bridi <ybridi@gmail.com>
2024-03-02 22:55:09 +0100

    lvim config

--- mac/lvim/.config/lvim/lua/user/keymaps.lua
+++ mac/lvim/.config/lvim/lua/user/keymaps.lua
@@ -1,98 +1,88 @@
--------------
--- Keymaps --
--------------
+lvim.keys.normal_mode["m"] = "<cmd>lua vim.diagnostic.goto_next()<cr>"
+lvim.keys.normal_mode["M"] = "<cmd>lua vim.diagnostic.goto_prev()<cr>"
 
--- Move betwen diagnostic
-lvim.keys.normal_mode["m"] = ":lua vim.diagnostic.goto_next()<CR>"
-lvim.keys.normal_mode["M"] = ":lua vim.diagnostic.goto_prev()<CR>"
+vim.cmd [[
+  vnoremap Y "ky
+  vnoremap P "kp
+  
+  nnoremap fo zo
+  nnoremap fc zc
+  nnoremap fO zR
+  nnoremap fC zM
+  
+  nnoremap gh ^
+  nnoremap gl $
+  vnoremap gh ^
+  vnoremap gl $
+  nnoremap k gk
+  nnoremap j gj
+  nnoremap <D-k> {
+  nnoremap <D-j> }
+  vnoremap <D-k> {
+  vnoremap <D-j> }
+  
+  nnoremap vil $v0
+  nnoremap yil $v0y
+  nnoremap dil $v0d
+  nnoremap cil $v0c
 
--- Select all
-lvim.keys.normal_mode["<leader>aa"] = "ggVG"
-
--- Resize Splits
--- Make adjusing split sizes a bit more friendly
-lvim.keys.normal_mode["<C-L>"] = ':vertical resize +3<CR>'
-lvim.keys.normal_mode["<C-J>"] = ':resize resize +3<CR>'
-lvim.keys.normal_mode["<C-K>"] = ':resize resize -3<CR>'
-lvim.keys.normal_mode["<C-H>"] = ':vertical resize -3<CR>'
-
--- Copy
-lvim.keys.visual_mode["Y"] = '"ky'
-lvim.keys.visual_mode["P"] = '"kp'
-
--- Moving around
--- lvim.keys.normal_mode["fl"] = "$"
--- lvim.keys.normal_mode["fh"] = "^"
--- lvim.keys.visual_mode["fl"] = "$"
--- lvim.keys.visual_mode["fh"] = "^"
-lvim.keys.normal_mode["k"] = "gk"
-lvim.keys.normal_mode["j"] = "gj"
-lvim.keys.normal_mode["<C-k>"] = "{"
-lvim.keys.normal_mode["<C-j>"] = "}"
-lvim.keys.visual_mode["<C-k>"] = "{"
-lvim.keys.visual_mode["<C-j>"] = "}"
-
--- Wording
-lvim.keys.normal_mode["yia"] = "yi'"
-lvim.keys.normal_mode["cia"] = "ci'"
-lvim.keys.normal_mode["dia"] = "di'"
-lvim.keys.normal_mode["via"] = "vi'"
-
-lvim.keys.normal_mode["yiq"] = 'yi"'
-lvim.keys.normal_mode["ciq"] = 'ci"'
-lvim.keys.normal_mode["diq"] = 'di"'
-lvim.keys.normal_mode["viq"] = 'vi"'
-
-lvim.keys.normal_mode["yi`"] = 'yi`'
-lvim.keys.normal_mode["ci`"] = 'ci`'
-lvim.keys.normal_mode["di`"] = 'di`'
-lvim.keys.normal_mode["vi`"] = 'vi`'
-
-lvim.keys.normal_mode["yie"] = 'yi{'
-lvim.keys.normal_mode["cie"] = 'ci{'
-lvim.keys.normal_mode["die"] = 'di{'
-lvim.keys.normal_mode["vie"] = 'vi{'
-
-lvim.keys.normal_mode["yil"] = '$v0y'
-lvim.keys.normal_mode["dil"] = '$v0d'
-lvim.keys.normal_mode["cil"] = '$v0c'
-lvim.keys.normal_mode["vil"] = '$v0'
-lvim.keys.normal_mode["gl"] = '$'
-lvim.keys.normal_mode["gh"] = '^'
-lvim.keys.visual_mode["gl"] = "$"
-lvim.keys.visual_mode["gh"] = "^"
-
--- Enter functionality
-lvim.keys.normal_mode["<leader><CR>"] = 'i<CR><Esc>'
-lvim.keys.normal_mode["<CR>"] = 'o<Esc>'
-
--- Get out of insert mode
-lvim.keys.command_mode["jk"] = '<C-C>'
-lvim.keys.insert_mode["jk"] = '<C-C>'
-
--- TODO: Source current file
-lvim.keys.normal_mode["<leader>y"] = ':w<cr> :luafile ~/.config/lvim/config.lua<cr>'
-
--- Surround
-lvim.keys.visual_mode["(("] = '"sc(<C-r>s)<Esc>'
-lvim.keys.visual_mode["{{"] = '"sc{<C-r>s}<Esc>'
-lvim.keys.visual_mode["[["] = '"sc[<C-r>s]<Esc>'
-lvim.keys.visual_mode['""'] = '"sc"<C-r>s"<Esc>'
-lvim.keys.visual_mode["''"] = '"sc\'<C-r>s\'<Esc>'
-lvim.keys.visual_mode["``"] = '"sc`<C-r>s`<Esc>'
-
--- Indent shortcuts
-lvim.keys.normal_mode["<Tab>"] = '>>_'
-lvim.keys.normal_mode["<S-Tab>"] = '<<_'
-lvim.keys.visual_mode["<Tab>"] = '>gv'
-lvim.keys.visual_mode["<S-Tab>"] = '<gv'
-
--- End macros in command mode
-lvim.keys.command_mode["<C-q>"] = '<C-f>i<C-o>q'
-
--- lvim.keys.normal_mode["K"] = '<cmd>lua vim.lsp.buf.hover()<CR>'
--- lvim.keys.normal_mode["gd"] = '<cmd>lua vim.lsp.buf.definition()<CR>'
-
--- Shift between buffers
-lvim.keys.normal_mode["H"] = ':bprev<cr>'
-lvim.keys.normal_mode["L"] = ':bnext<cr>'
+  nnoremap yia yi'
+  nnoremap cia ci'
+  nnoremap dia di'
+  nnoremap via vi'
+  
+  nnoremap yiq yi"
+  nnoremap ciq ci"
+  nnoremap diq di"
+  nnoremap viq vi"
+  
+  nnoremap yi` yi`
+  nnoremap ci` ci`
+  nnoremap di` di`
+  nnoremap vi` vi`
+  
+  nnoremap yii yi{
+  nnoremap cii ci{
+  nnoremap dii di{
+  nnoremap vii vi{
+  
+  nnoremap yin yi<
+  nnoremap cin ci<
+  nnoremap din di<
+  nnoremap vin vi<
+  
+  nmap <C-a> ggVG
+  
+  inoremap <S-j> <Down>
+  inoremap <S-k> <UP>
+  
+  nnoremap <leader>w :w<cr>
+  nnoremap <leader>q :q<cr>
+  
+  nmap <leader><CR> i<CR><Esc>
+  nmap <CR> o<Esc>
+  
+  imap jk <Esc>
+  cmap jk <C-C>
+  
+  vnoremap (( "sc(<C-r>s)<Esc>
+  vnoremap {{ "sc{<C-r>s}<Esc>
+  vnoremap [[ "sc[<C-r>s]<Esc>
+  vnoremap "" "sc"<C-r>s"<Esc>
+  vnoremap '' "sc'<C-r>s'<Esc>
+  vnoremap `` "sc`<C-r>s`<Esc>
+  
+  noremap <Tab> >>
+  noremap <S-tab> <<
+  
+  map <Leader>n *
+  map <Leader>p #
+  
+  autocmd VimEnter * map <S-h> :bp<CR>
+  autocmd VimEnter * map <S-l> :bn<CR>
+  noremap <leader>s :bd<CR>
+  
+  nnoremap <leader>u za
+  
+  cnoremap <c-q> <c-f>i<c-o>q
+]]
--- mac/lvim/.config/lvim/lua/user/plugins.lua
+++ mac/lvim/.config/lvim/lua/user/plugins.lua
@@ -1,31 +1,63 @@
+-------------------
+--- All Plugins ---
+-------------------
 lvim.plugins = {
-  { "jose-elias-alvarez/nvim-lsp-ts-utils", requires = { "jose-elias-alvarez/null-ls.nvim" } },
-  { "windwp/nvim-ts-autotag" },
-  { "terryma/vim-multiple-cursors" },
-  { "norcalli/nvim-colorizer.lua" },
-  { "ellisonleao/gruvbox.nvim" },
-  { "rmagatti/auto-session" },
-  { "ray-x/lsp_signature.nvim" },
-  { "jose-elias-alvarez/typescript.nvim" },
-  { "folke/todo-comments.nvim",             requires = { "nvim-lua/plenary.nvim" } },
-  { 'junegunn/fzf',                         dir = '~/.fzf',                                  run = './install --all' },
-  { "junegunn/fzf.vim" },
+  { "lunarvim/colorschemes" },
+  { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true},
   {
-    "gelfand/copilot.vim",
-    disable = not lvim.builtin.sell_soul_to_devel,
-    config = function()
-      -- copilot assume mapped
-      vim.g.copilot_assume_mapped = true
-      vim.g.copilot_no_tab_map = true
-    end
-  },
-  {
-    "hrsh7th/cmp-copilot",
-    disable = not lvim.builtin.sell_soul_to_devel,
-    config = function()
-      lvim.builtin.cmp.formatting.source_names["copilot"] = "(Cop)"
-      table.insert(lvim.builtin.cmp.sources, { name = "copilot" })
-    end
+    "folke/persistence.nvim",
+      event = "BufReadPre",
+      config = function()
+        require("persistence").setup {
+          dir = vim.fn.expand(vim.fn.stdpath "config" .. "/session/"),
+          options = { "buffers", "curdir", "tabpages", "winsize" },
+        }
+    end,
   },
 }
--- use '/opt/homebrew/bin/fzf'
+
+----------------------
+--- Copilot Plugin ---
+----------------------
+table.insert(lvim.plugins, {
+  "zbirenbaum/copilot-cmp",
+  event = "InsertEnter",
+  dependencies = { "zbirenbaum/copilot.lua" },
+  config = function()
+    vim.defer_fn(function()
+      require("copilot").setup() -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+      require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+    end, 100)
+  end,
+})
+
+--------------------------
+--- Session Management ---
+--------------------------
+local persistenceGroup = vim.api.nvim_create_augroup("Persistence", { clear = true })
+local home = vim.fn.expand "~"
+local disabled_dirs = {
+  home,
+  home .. "/Downloads",
+  "/private/tmp",
+}
+
+-- disable persistence for certain directories
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+  group = persistenceGroup,
+  callback = function()
+    local cwd = vim.fn.getcwd()
+    for _, path in pairs(disabled_dirs) do
+      if path == cwd then
+        require("persistence").stop()
+        return
+      end
+    end
+    if vim.fn.argc() == 0 and not vim.g.started_with_stdin then
+      require("persistence").load()
+    else
+      require("persistence").stop()
+    end
+  end,
+  nested = true,
+})
