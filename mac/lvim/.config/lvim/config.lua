9db0fd7fc43cde7596bce1a119ae136705accda5
Yassine Bridi <ybridi@gmail.com>
2024-03-02 22:55:09 +0100

    lvim config

--- mac/lvim/.config/lvim/config.lua
+++ mac/lvim/.config/lvim/config.lua
@@ -1,8 +1,9 @@
-require('user/telescope')
-require('user/keymaps')
--- require('user/lsp')
-require('user/macros')
-require('user/which-key')
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
 require('user/settings')
+require('user/keymaps')
+require('user/macros')
 require('user/plugins')
-require('user/plugins-settings')
--- mac/lvim/.config/lvim/lua/user/macros.lua
+++ mac/lvim/.config/lvim/lua/user/macros.lua
@@ -1,19 +1,14 @@
-------------
--- Macros --
-------------
-
--- Typescript
--- Surround with trycatch
--- lvim.keys.visual_mode["<leader>tr"] = '>gvctry {}kpk%a catch(error){console.log(error)}'
-
--- -- JSX Tag completion
-lvim.keys.insert_mode["<M-tab>"] = '<esc>yiwi<<esc>ea></<esc>pa><esc>bba'
-
--- -- JSX Tag completion without closing tag
-lvim.keys.insert_mode["<C-y>"] = '<esc>bi<<esc>ea /><esc>'
-
--- -- Enclose a bunch of tags with a div Tag
-lvim.keys.visual_mode["<leader>tg"] = 'dOdiv<esc><esc>"ayiwi<ea></"apa>bbaPka'
-
--- -- Console.log macos
-lvim.keys.normal_mode["<leader>lt"] = "yiwoconsole.log('<esc>pa: ', <esc>pa);<esc>"
+vim.cmd [[
+  vnoremap <leader>tr >gvctry {}kpk%a catch(error){console.log(error)}
+                    
+  let @k = 'vbd%pldwhyiwPldei'
+  inoremap <C-b> <esc>@k
+  
+  inoremap <C-x> <esc>yiwi<ea></pa>bba
+  
+  vnoremap <leader>tg dOdiv<esc><esc>"ayiwi<ea></"apa>bbaPka
+          
+  inoremap <C-l> <esc>diw[(opbi<ea>[(%Opbi</ea>
+  
+  nnoremap <leader>lt yiwoconsole.log('pa: a', pa);
+]]
