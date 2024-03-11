baf808a4cab1cb66e5727e3d61893a9557279213
Yassine Bridi <ybridi@gmail.com>
2024-03-11 00:33:15 +0000

    new lunarvim settings, plugins

--- mac/lvim/.config/lvim/lua/user/settings.lua
+++ mac/lvim/.config/lvim/lua/user/settings.lua
@@ -16,6 +16,9 @@ lvim.builtin.which_key.mappings["f"] = { "<cmd>FzfLua files<cr>", "Find File" }
 lvim.builtin.which_key.mappings["rg"] = { "<cmd>FzfLua grep_visual<cr>", "Grep" }
 lvim.builtin.which_key.mappings["aa"] = { "<cmd>normal! ggVG<cr>", "Select All" }
 lvim.builtin.which_key.mappings["<cr>"] = { "i<cr><esc>", "New Line" }
+lvim.builtin.which_key.mappings["lA"] = { function() vim.lsp.buf.code_action({ context = { only = { "source" }, diagnostics = {}, }, }) end,
+  "Source Action" }
+
 
 vim.cmd [[
   au BufWritePost *yabairc !brew services restart koekeishiya/formulae/yabai
