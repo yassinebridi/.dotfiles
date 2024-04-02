36a6587d06a4a3934275e84fdfc9c71250749e3c
Yassine Bridi <ybridi@gmail.com>
2024-04-02 01:27:59 +0000

    better neovim mappings

--- mac/lvim/.config/lvim/lua/user/settings.lua
+++ mac/lvim/.config/lvim/lua/user/settings.lua
@@ -5,6 +5,8 @@ lvim.colorscheme = "gruvbox"
 lvim.transparent_window = true
 lvim.builtin.alpha.active = false
 lvim.builtin.lualine.style = "default" -- or "none"
+lvim.builtin.cmp.mapping["<Tab>"] = vim.NIL
+lvim.builtin.cmp.mapping["<S-Tab>"] = vim.NIL
 
 lvim.lsp.buffer_mappings.normal_mode["gl"] = nil
 -- use treesitter folding
@@ -12,16 +14,10 @@ vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevelstart = 99
 vim.opt.whichwrap = "b,s"
+vim.opt.wrap = true
+vim.opt.linebreak = true
 -- vim.opt.cmdheight = 2
 lvim.lsp.installer.setup.automatic_installation = true
-lvim.builtin.which_key.mappings["f"] = { "<cmd>FzfLua files<cr>", "Find File" }
-lvim.builtin.which_key.mappings["rg"] = { "<cmd>FzfLua grep_visual<cr>", "Grep" }
-lvim.builtin.which_key.mappings["aa"] = { "<cmd>normal! ggVG<cr>", "Select All" }
-lvim.builtin.which_key.mappings["<cr>"] = { "i<cr><esc>", "New Line" }
-lvim.builtin.which_key.mappings["h"] = { "<cmd>lua require'mywords'.hl_toggle()<cr>", "Hightlight Word" }
-lvim.builtin.which_key.mappings["u"] = { "<cmd>lua require'mywords'.uhl_all()<cr><cmd>nohlsearch<cr>", "Unhightlight All" }
-lvim.builtin.which_key.mappings["lA"] = { function() vim.lsp.buf.code_action({ context = { only = { "source" }, diagnostics = {}, }, }) end,
-  "Source Action" }
 
 
 vim.cmd [[
