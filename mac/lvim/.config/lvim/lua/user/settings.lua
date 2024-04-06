f175ef32f1393b7dbf45172c256d565d8794b119
Yassine Bridi <ybridi@gmail.com>
2024-04-06 23:42:04 +0000

    neovim: macros, prettier, snippets

--- mac/lvim/.config/lvim/lua/user/settings.lua
+++ mac/lvim/.config/lvim/lua/user/settings.lua
@@ -24,3 +24,16 @@ vim.cmd [[
   au BufWritePost *yabairc !brew services restart koekeishiya/formulae/yabai
   au BufWritePost *skhdrc !brew services restart skhd
 ]]
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  {
+    name = "prettier",
+    ---@usage arguments to pass to the formatter
+    -- these cannot contain whitespace
+    -- options such as `--line-width 80` become either `{"--line-width", "80"}` or `{"--line-width=80"}`
+    args = { "--print-width", "100" },
+    ---@usage only start in these filetypes, by default it will attach to all filetypes it supports
+    filetypes = { "typescript", "typescriptreact" },
+  },
+}
