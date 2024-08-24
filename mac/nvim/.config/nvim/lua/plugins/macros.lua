194a6c107f7e95160adc32352d4c46314f633ba0
Yassine Bridi <ybridi@gmail.com>
2024-08-24 21:11:04 +0100

    astronvim: better mappings, and macros

--- mac/nvim/.config/nvim/lua/plugins/macros.lua
+++
@@ -1,9 +0,0 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
-return {
-  vim.cmd [[
-    inoremap <C-l> <esc>diw[(opbi<ea>[(%Opbi</ea>
-    inoremap <C-x> <esc>yiwi<ea></pa>bba
-    nnoremap <leader>lt yiwoconsole.log('pa: a', pa);
-  ]]
-}
--- mac/nvim/.config/nvim/lua/plugins/mappings.lua
+++ mac/nvim/.config/nvim/lua/plugins/mappings.lua
@@ -1,55 +1,6 @@
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 return {
-  vim.cmd [[
-    vnoremap Y "ky
-    vnoremap P "kp
-
-    nnoremap gh ^
-    nnoremap gl $
-    vnoremap gh ^
-    vnoremap gl $
-    nnoremap k gk
-    nnoremap j gj
-
-    nmap <CR> o<space><Esc>
-
-    vnoremap (( "sc(<C-r>s)<Esc>
-    vnoremap {{ "sc{<C-r>s}<Esc>
-    vnoremap [[ "sc[<C-r>s]<Esc>
-    vnoremap "" "sc"<C-r>s"<Esc>
-    vnoremap '' "sc'<C-r>s'<Esc>
-    vnoremap `` "sc`<C-r>s`<Esc>
-
-    nnoremap yia yi'
-    nnoremap cia ci'
-    nnoremap dia di'
-    nnoremap via vi'
-
-    nnoremap yiq yi"
-    nnoremap ciq ci"
-    nnoremap diq di"
-    nnoremap viq vi"
-
-    nnoremap yi` yi`
-    nnoremap ci` ci`
-    nnoremap di` di`
-    nnoremap vi` vi`
-
-    noremap <Tab> >>
-    noremap <S-tab> <<
-
-    map <Leader>n *
-    map <Leader>p #
-
-    autocmd VimEnter * map <S-h> :bp<CR>
-    autocmd VimEnter * map <S-l> :bn<CR>
-
-    nmap <leader><CR> i<CR><Esc>
-    cnoremap <c-q> <c-f>i<c-o>q
-
-    nnoremap <leader>aa <cmd>normal! ggVG<cr>
-  ]],
   {
     "AstroNvim/astrocore",
     ---@type AstroCoreOpts
@@ -57,13 +8,58 @@ return {
       mappings = {
         n = {
           ["<Leader>aa"] = { "<cmd>normal! ggVG<cr>", desc = "Select All" },
-          ["gl"] = false,
           ["<Leader>h"] = { "<cmd>lua require'mywords'.hl_toggle()<cr>", desc = "Hightlight Word" },
           ["<Leader>u"] = { "<cmd>lua require'mywords'.uhl_all()<cr><cmd>nohlsearch<cr>", desc = "Unhightlight All" },
-          -- vim.keymap.set("n", "<leader>k", '<cmd>lua require("kubectl").open()<cr>', { noremap = true, silent = true })
           ["<Leader>k"] = { "<cmd>lua require'kubectl'.open()<cr>", desc = "Kubectl" },
-          ["<Leader>rr"] = { "<cmd>Resurrect<cr>", desc = "Kubectl" },
+          ["<Leader>rr"] = { "<cmd>Resurrect<cr>", desc = "Resurrect" },
           ["<Leader>dn"] = { "<cmd>lua require'notify'.dismiss()<cr>", desc = "Dismiss Notifications" },
+          ["Y"] = { '"ky', desc = "Yank to k register" },
+          ["P"] = { '"kp', desc = "Paste from k register" },
+          ["gh"] = { "^", desc = "Go to start of line" },
+          ["gl"] = { "$", desc = "Go to end of line" },
+          ["<CR>"] = { "o<space><Esc>", desc = "New line with space" },
+          ["yia"] = { "yi'", desc = "Yank inside single quotes" },
+          ["cia"] = { "ci'", desc = "Change inside single quotes" },
+          ["dia"] = { "di'", desc = "Delete inside single quotes" },
+          ["via"] = { "vi'", desc = "Select inside single quotes" },
+          ["yiq"] = { 'yi"', desc = "Yank inside double quotes" },
+          ["ciq"] = { 'ci"', desc = "Change inside double quotes" },
+          ["diq"] = { 'di"', desc = "Delete inside double quotes" },
+          ["viq"] = { 'vi"', desc = "Select inside double quotes" },
+          ["yi`"] = { "yi`", desc = "Yank inside backticks" },
+          ["ci`"] = { "ci`", desc = "Change inside backticks" },
+          ["di`"] = { "di`", desc = "Delete inside backticks" },
+          ["vi`"] = { "vi`", desc = "Select inside backticks" },
+          ["<Tab>"] = { ">>", desc = "Indent" },
+          ["<S-Tab>"] = { "<<", desc = "Unindent" },
+          ["<Leader>n"] = { "*", desc = "Search word under cursor forward" },
+          ["<Leader>p"] = { "#", desc = "Search word under cursor backward" },
+          ["<S-h>"] = { ":bp<CR>", desc = "Previous buffer" },
+          ["<S-l>"] = { ":bn<CR>", desc = "Next buffer" },
+          ["<leader><CR>"] = { "i<CR><Esc>", desc = "Insert newline" },
+          ["<Leader>lt"] = { "yiwoconsole.log('pa: a', pa);", desc = "Insert console.log" }, -- macro
+        },
+        i = {
+          ["<c-l>"] = { "<esc>diw[(opbi<ea>[(%Opbi</ea>", desc = "Insert JSX tags" }, -- macro
+          ["<c-x>"] = { "<esc>yiwi<ea></pa>bba", desc = "Insert JSX tags" }, -- macro
+        },
+        v = {
+          ["Y"] = { '"ky', desc = "Yank to k register" },
+          ["P"] = { '"kp', desc = "Paste from k register" },
+          ["gh"] = { "^", desc = "Go to start of line" },
+          ["gl"] = { "$", desc = "Go to end of line" },
+          ["(("] = { '"sc(<C-r>s)<Esc>', desc = "Surround with parentheses" },
+          ["{{"] = { '"sc{<C-r>s}<Esc>', desc = "Surround with curly braces" },
+          ["[["] = { '"sc[<C-r>s]<Esc>', desc = "Surround with square brackets" },
+          ['""'] = { '"sc"<C-r>s"<Esc>', desc = "Surround with double quotes" },
+          ["''"] = { "\"sc'<C-r>s'<Esc>", desc = "Surround with single quotes" },
+          ["``"] = { '"sc`<C-r>s`<Esc>', desc = "Surround with backticks" },
+          ["<Tab>"] = { ">>", desc = "Indent" },
+          ["<S-Tab>"] = { "<<", desc = "Unindent" },
+          ["<Leader>tr"] = { ">gvctry {\n}kpk%a catch(error){\nconsole.log(error)\n}", desc = "Try Catch" }, -- macro
+        },
+        c = {
+          ["<c-q>"] = { "<c-f>i<c-o>q", desc = "Open command-line window" },
         },
       },
     },
