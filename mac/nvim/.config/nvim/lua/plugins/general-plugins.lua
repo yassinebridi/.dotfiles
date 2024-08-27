66de3e3934432602e67d3553faa4534be0082aeb
Yassine Bridi <ybridi@gmail.com>
2024-08-27 23:20:56 +0100

    neovim: avante for AI

--- mac/nvim/.config/nvim/lua/plugins/general-plugins.lua
+++ mac/nvim/.config/nvim/lua/plugins/general-plugins.lua
@@ -42,4 +42,53 @@ return {
   {
     "supercrabtree/vim-resurrect",
   },
+  -- {
+  --   "olimorris/codecompanion.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "nvim-telescope/telescope.nvim",
+  --     {
+  --       "stevearc/dressing.nvim",
+  --       opts = {},
+  --     },
+  --   },
+  --   config = function()
+  --     require("codecompanion").setup {
+  --       strategies = {
+  --         chat = {
+  --           adapter = "anthropic",
+  --         },
+  --         inline = {
+  --           adapter = "anthropic",
+  --         },
+  --         agent = {
+  --           adapter = "anthropic",
+  --         },
+  --       },
+  --     }
+  --   end,
+  -- },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    build = "make", -- This is Optional, only if you want to use tiktoken_core to calculate tokens count
+    opts = {
+      -- add any opts here
+    },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below is optional, make sure to setup it properly if you have lazy=true
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
 }
