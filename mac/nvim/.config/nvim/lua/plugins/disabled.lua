5fcd0b8bb50fe12f8ffca704da8ccc7acdd22dc9
Yassine Bridi <yassinebridi@MacBook-Pro-AppleOsX-1500.local>
2024-11-04 16:59:33 +0100

    Linux branch

--- mac/nvim/.config/nvim/lua/plugins/disabled.lua
+++
@@ -1,5 +0,0 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
-return {
-  { "rcarriga/nvim-notify", enabled = false },
-}
--- mac/yazi/.config/yazi/init.lua
+++
@@ -1,2 +0,0 @@
-require("no-status"):setup()
-require("full-border"):setup()
