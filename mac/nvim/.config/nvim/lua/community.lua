35409bc2eed0c51962d236894181774178510619
Yassine Bridi <ybridi@gmail.com>
2025-02-11 13:48:27 +0100

    yok script, typescript astronvim

--- mac/nvim/.config/nvim/lua/community.lua
+++ mac/nvim/.config/nvim/lua/community.lua
@@ -15,6 +15,7 @@ return {
   { import = "astrocommunity.colorscheme.github-nvim-theme" },
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  { import = "astrocommunity.pack.typescript" },
   -- { import = "astrocommunity.pack.typescript" },
   -- import/override with your plugins folder
 }
