a1906df8c49ebb08a0fe28cd5fe4f1f01604157d
Yassine Bridi <ybridi@gmail.com>
2025-04-24 22:55:47 +0100

    neovim config

--- mac/nvim/.config/nvim/lua/plugins/astrocore.lua
+++ mac/nvim/.config/nvim/lua/plugins/astrocore.lua
@@ -57,5 +57,32 @@ return {
         -- This can be found in the `lua/lazy_setup.lua` file
       },
     },
+    mappings = {
+      -- first key is the mode
+      n = {
+        -- second key is the lefthand side of the map
+
+        -- navigate buffer tabs
+        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+
+        -- mappings seen under group name "Buffer"
+        ["<Leader>bd"] = {
+          function()
+            require("astroui.status.heirline").buffer_picker(
+              function(bufnr) require("astrocore.buffer").close(bufnr) end
+            )
+          end,
+          desc = "Close buffer from tabline",
+        },
+
+        -- tables with just a `desc` key will be registered with which-key if it's installed
+        -- this is useful for naming menus
+        -- ["<Leader>b"] = { desc = "Buffers" },
+
+        -- setting a mapping to false will disable it
+        -- ["<C-S>"] = false,
+      },
+    },
   },
 }
--- mac/nvim/.config/nvim/lua/plugins/astrolsp.lua
+++ mac/nvim/.config/nvim/lua/plugins/astrolsp.lua
@@ -33,7 +33,7 @@ return {
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
       },
-      timeout_ms = 1000, -- default format timeout
+      timeout_ms = 2000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end
@@ -41,6 +41,7 @@ return {
     -- enable servers that you already have installed without mason
     servers = {
       -- "pyright"
+      -- "prettierd"
     },
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
